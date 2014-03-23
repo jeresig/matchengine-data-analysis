570faa5bc776b4bc11ddb2f165cc9046a84807d3
John Resig <jeresig@gmail.com>
2014-03-23 18:06:25 -0400

    Update utility for generating the known frick-zeri map.

--- utils/zeri/gen-known-map.js
+++ utils/zeri/gen-known-map.js
@@ -2,12 +2,15 @@ var fs = require("fs");
 var libxmljs = require("libxmljs");
 var csv = require("csv-streamify");
 var _ = require("lodash");
+var through = require("through");
 
 var map = {};
 
 var csvFile = process.argv[3] || "frick-zeri.tsv";
-var file = process.argv[2] || "fzeri_F_2014_02_10_105226.xml";
+var file = process.argv[2] || "fzeri_OA_2014_03_11_132558.xml";
+var outFile = "frick-zeri-map.csv";
 
+var outStream = fs.createWriteStream(outFile);
 var xmlDoc = libxmljs.parseXml(fs.readFileSync(file, "utf8"));
 
 fs.createReadStream(csvFile)
@@ -15,32 +18,36 @@ fs.createReadStream(csvFile)
         objectMode: true,
         delimiter: "\t"
     }))
-    .on("data", function(data) {
-        if (data[0] && data[2]) {
-            var fid = data[0];
-            var results = [];
-
-            data[2].split(/,\s*/).forEach(function(id) {
-                var node = xmlDoc.get("//SCHEDA[@sercdf='" + id + "']");
-
-                if (node) {
-                    var artworkID = node.attr("sercdoa").value();
-                    results.push(artworkID);
-
-                    /* // Uncomment to output image file names instead
-                    var path = node.get(".//FOTO").text();
-                    var imageID = /\/([\w\d]+).jpg/.exec(path)[1];
-
-                    results.push(imageID);
-                    */
-                } else {
-                    //console.log("NOT FOUND", id)
-                }
-            });
-
-            if (results.length > 0) {
-                results = _.uniq(results.sort());
-                console.log([fid].concat(results).join(","));
-            }
+    .pipe(through(function(data) {
+        if (!data[0] || !data[1]) {
+            return;
         }
-    });
+
+        var fid = data[0];
+        var results = [];
+
+        data[1].split(/,\s*/).forEach(function(id) {
+            var node = xmlDoc.get("//SCHEDA[.//NRSCHEDA[text()='" + id + "']]");
+
+            if (node) {
+                var artworkID = node.attr("sercdoa").value();
+                results.push(artworkID);
+
+                /* // Uncomment to output image file names instead
+                var path = node.get(".//FOTO").text();
+                var imageID = /\/([\w\d]+).jpg/.exec(path)[1];
+
+                results.push(imageID);
+                */
+                console.log("FOUND", id);
+            } else {
+                console.log("NOT FOUND", id);
+            }
+        });
+
+        if (results.length > 0) {
+            results = _.uniq(results.sort());
+            this.queue([fid].concat(results).join(",") + "\n");
+        }
+    }))
+    .pipe(outStream);
